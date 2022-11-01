2dbb02ec818118837fb85888175cfe6e391d4b4a
Mariusz Zając <zajac@gmx.us>
2022-11-01 19:59:20 +0100

    Add annotation

--- Uber/Core/Home/View/HomeView.swift
+++ Uber/Core/Home/View/HomeView.swift
@@ -13,7 +13,7 @@ struct HomeView: View {
     
     var body: some View {
         ZStack (alignment: .top){
-            UberAppViewRepresentable()
+            UberMapViewRepresentable()
                 .ignoresSafeArea()
             if showLocationSearchView {
                 LocationSearchView(showLocationSearchView: $showLocationSearchView
--- Uber/Core/Home/View/UberMapViewRepresentable.swift
+++ Uber/Core/Home/View/UberMapViewRepresentable.swift
@@ -1,5 +1,5 @@
 //
-//  UberAppViewRepresentable.swift
+//  UberMapViewRepresentable.swift
 //  Uber
 //
 //  Created by Mariusz Zając on 31/10/2022.
@@ -8,7 +8,8 @@
 import SwiftUI
 import MapKit
 
-struct UberAppViewRepresentable: UIViewRepresentable {
+struct UberMapViewRepresentable: UIViewRepresentable {
+    
     let mapView = MKMapView()
     let locationManager = LocationManager()
     @EnvironmentObject var locationViewModel: LocationSearchViewModel
@@ -18,11 +19,13 @@ struct UberAppViewRepresentable: UIViewRepresentable {
         mapView.isRotateEnabled = false
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
+        
         return mapView
+        
     }
     func updateUIView(_ uiView: UIViewType, context: Context) {
         if let coordinate = locationViewModel.selectedLocationCoordinate {
-            print("DEBUG: Selected location in map view \(coordinate)")
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
         }
     }
     func makeCoordinator() -> MapCoordinator {
@@ -31,23 +34,41 @@ struct UberAppViewRepresentable: UIViewRepresentable {
     
     
 }
-extension UberAppViewRepresentable {
+extension UberMapViewRepresentable {
     
     class MapCoordinator: NSObject, MKMapViewDelegate {
-        let parent:UberAppViewRepresentable
         
-        init(parent: UberAppViewRepresentable) {
+        //MARK: -properties
+        let parent:UberMapViewRepresentable
+        
+        //MARK: -Lifecycle
+        
+        init(parent: UberMapViewRepresentable) {
             self.parent = parent
             super.init()
         }
+        //MARK: -MKMapVievDelegate
+        
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
             let region = MKCoordinateRegion(
                 center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
                 span: MKCoordinateSpan(
-                    latitudeDelta: 10, //for more accurity set bouth value 0.05  for test =10 TODO
-                    longitudeDelta: 10))
+                    latitudeDelta: 0.05, //for more accurity set bouth value 0.05  for test =10 TODO
+                    longitudeDelta: 0.05))
             
             parent.mapView.setRegion(region, animated: true)
+        }
+        //MARK: - Helpers
+        
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            parent.mapView.addAnnotation(anno)
+            parent.mapView.selectAnnotation(anno, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
         }
     }
 }
