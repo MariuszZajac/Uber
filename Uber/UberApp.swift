59e5ea0d5a9b738cd6b9d62deee261f2a4b19ee3
Mariusz Zając <zajac@gmx.us>
2022-11-01 14:11:09 +0100

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Mariusz Zając on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
